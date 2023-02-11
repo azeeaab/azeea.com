36bb3f442cc8251c4992543f4f15cc9076714e6c
Johan Segerfeldt <johan.segerfeldt@me.com>
2023-02-11 17:25:06 +0100

    Add Flow prototypes

---
+++ FlowPilot/eddie.md
@@ -0,0 +1,14 @@
+abstrahera webcrawler information att koppla in till flow-uppläsning:
+Plats
+Tid (visas ej om inte öppet/säsongsbetonat/öppetider)
+Besökare (visas ej om det inte finns besökare)
+Bild (visas från befintlig information)
+
+Läsa in smultron-koordinater utifrån rörlig koordinat (er)
+Läsa in transport-sträckor utifrån rörlig koordinat
+Bot-föreslå i realtid mellan aktörer på väg mot gemensam koordinat för aktivitet
+Bot-föreslå tid-uppskattning för respektive aktöre gällandes EST 
+Bot-föreslå transport-aktör-gemensamma smultron-EST förslag i samband mot mål.
+Bot-föreslå alternativa rörliga mål baserat på gemensamma smultron-intressen, optimal
+proximitet för flertalet aktörer och nyhetsvärden, intressen för respektive aktör
+samt nyhetsvärden generellt för communityt samt företeelse historik.
---
+++ FlowPilot/src/falling-image.js
@@ -0,0 +1,41 @@
+const IMAGE_POSITION_TIME = 2000
+
+class FallingImageElement {
+  element
+  isDismissed = false
+
+  constructor(element) {
+    this.element = element
+  }
+
+  positionAtTop() {
+    if (this.isDismissed) return
+
+    this.element.style.top = '-2vh'
+    this.element.style.left = `${600 + randomInt(innerWidth - 850)}px`
+    this.element.style.opacity = '0'
+  }
+
+  async positionInCenter() {
+    if (this.isDismissed) return
+
+    this.element.style.top = '47vh'
+    this.element.style.opacity = '1'
+    await delay(IMAGE_POSITION_TIME)
+  }
+  
+  async positionAsSelected() {
+    this.element.style.top = '75vh'
+    this.element.style.left = '15vw'
+    await delay(IMAGE_POSITION_TIME)
+  }
+
+  async dismiss() {
+    this.isDismissed = true
+    this.element.onmousedown = null
+    this.element.style.top = '150vh'
+    this.element.style.opacity = '0'
+    await delay(IMAGE_POSITION_TIME)
+    this.element.remove()
+  }
+}
