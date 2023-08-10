d337a061cf46a68c7543301e8262371b6e142ba0
Johan Segerfeldt <johan.segerfeldt@me.com>
2023-08-10 11:17:03 +0200

    Add slider_state

--- FlowPilot/src_6/slider.js
+++ FlowPilot/src_6/slider.js
@@ -2,23 +2,25 @@ const minX = 24
 const maxX = 473
 const tabHalfWidth = 2.5
 
-var lastMouseDownCoords
+const slider_state = {
+  dragOrigin: null,
+}
 
 function startDragSliderTab(e) {
   const {pageX, pageY} = e
-  lastMouseDownCoords = {pageX, pageY}
+  slider_state.dragOrigin = {pageX, pageY}
   e.target.setPointerCapture(e.pointerId)
 }
 
 function dragSliderTab(e) {
-  if (!lastMouseDownCoords) return
+  if (!slider_state.dragOrigin) return
 
   const {pageX} = e
   tab().style.left = `${Math.min(Math.max(pageX - sliderBoundingRect().x - tabHalfWidth, minX), maxX)}px`
 }
 
 function stopDragSliderTab(e) {
-  lastMouseDownCoords = null
+  slider_state.dragOrigin = null
   e.target.releasePointerCapture(e.pointerId)
 }
 
@@ -34,10 +36,8 @@ function tab() {
   return sliderBackground().getElementsByClassName('tab')[0]
 }
 
-// TODO: There should be 3 of each button and main area (A, B, C)
 // TODO: Also: Switch all buttons/images when switching avatar
 // TODO: Refactor where state is stored
-// TODO: Does not work on Edge...? Replace <video> with an <img>?
 
 function displaySearch() {
   searchSButton().style.display = 'none'
